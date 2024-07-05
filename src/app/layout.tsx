050b0dd9a8916b1ad9f8f61dbfcff8e29b31ece4
mayank2130 <mayankthakur1712@gmail.com>
2024-07-05 23:29:46 +0530

    create chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Recursive } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
+import { Toaster } from "react-hot-toast";
 
 const inter = Recursive({ subsets: ["latin"] });
 
@@ -16,10 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
+    <Providers>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+        <Toaster />
+      </html>
+    </Providers>
   );
 }
